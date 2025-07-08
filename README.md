7fe490607343c8d6e7bf67f10e6face47264fb7f
Suman Dey <140014618+sumandey2023@users.noreply.github.com>
2025-07-08 12:40:52 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+backend server start -> npm run dev
+frontend server start -> npm run dev
+database password -> bsprojects
+
+backend .env
+.env {
+PORT = 3000
+MONGO_URI = mongodb+srv://bsprojects001:bsprojects@booklyst.7javu7t.mongodb.net/?retryWrites=true&w=majority&appName=booklyst
+}
---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./src/db/connectdb");
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+app.use(express.json());
+//  connect to the database
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
