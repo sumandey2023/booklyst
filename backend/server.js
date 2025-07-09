110061751f79cb09d15a6e8833d66d5e9d060765
Suman Dey <140014618+sumandey2023@users.noreply.github.com>
2025-07-09 14:38:35 +0530

    final start

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,6 @@ const PORT = process.env.PORT;
 app.use(express.json());
 //  connect to the database
 connectDB();
-console.log("suman");
-console.log("hi");
-console.log("hi");
-console.log("hi");
-console.log("test69");
-console.log("test54464");
 
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
